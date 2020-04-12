14b3ac200dc023b85ab59f7d24b01331cca1ab2b
Tung Duong <laptung051@gmail.com>
2020-04-12 21:12:54 +0700

    Create Animation for Label Text in Welcome Screen

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,8 +8,17 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+            
+        titleLabel.text = "";
+        let titleText = "⚡️FlashChat";
+        var charIdx = 0.0;
+        
+        for letter in titleText{
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIdx, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter);
+            }
+            charIdx+=1.5;
+        }
     }
     
 
